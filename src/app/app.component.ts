76ff03ed07d0e46ce10d9fe45d221dc692d4e700
luisperezlec <luisperezlec@gmail.com>
2024-10-03 23:39:54 -0600

    Initial setup for PrimeNG 18b

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+// PrimeNG
+import { PrimeNGConfig } from 'primeng/api';
+import { Aura } from 'primeng/themes/aura';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    // PrimeNG components
+    ButtonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'primeng18';
+
+  constructor(private config: PrimeNGConfig) {
+    this.config.theme.set({ preset: Aura });
+  }
+
+  ngOnInit() {
+    this.config.ripple.set(true);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+// Animations for PrimeNG
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync()
+  ]
 };
