579bf20cd159418d9a48dd5e3f959c13d3c47b62
LuisPerezLec <luisperezlec@gmail.com>
2024-10-04 12:48:06 -0600

    Sidebar toggle

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 
+// PrimeNG
+import { DrawerModule } from 'primeng/drawer';
+import { ButtonModule } from "primeng/button";
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    DrawerModule,
+    ButtonModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  visible: boolean = false;
 }
