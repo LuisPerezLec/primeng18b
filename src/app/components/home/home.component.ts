bb2115408242063fd439048348c81404192eb7a4
luisperezlec <luisperezlec@gmail.com>
2024-10-05 02:24:02 -0600

    Configuración genérica del sidebar (hardcoded - sin modal mask)

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { NgClass, NgIf } from '@angular/common';
 
 // PrimeNG
 import { DrawerModule } from 'primeng/drawer';
@@ -9,11 +10,38 @@ import { ButtonModule } from "primeng/button";
   standalone: true,
   imports: [
     DrawerModule,
-    ButtonModule
+    ButtonModule,
+    NgClass,
+    NgIf
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
   visible: boolean = false;
+  screenWidth: number = window.innerWidth;
+
+  ngOnInit() {
+    this.checkSidebarVisibility();
+  }
+
+  // Escuchar cambio de tamaño en pantalla
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.screenWidth = event.target.innerWidth;
+    this.checkSidebarVisibility();
+  }
+
+  toggleSidebar() {
+    this.visible = !this.visible;
+  }
+
+  isVisible(): boolean {
+    return this.screenWidth < 992; // Cambia este valor según tus breakpoints
+  }
+
+  checkSidebarVisibility() {
+    // Establece la visibilidad inicial según el tamaño de la pantalla
+    this.visible = this.screenWidth >= 992; // Visibilidad true si es lg o más
+  }
 }
